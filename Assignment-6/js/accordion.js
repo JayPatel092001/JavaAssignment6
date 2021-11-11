6485c9837b49233ef430b31eb5083e1113891f78
Jay Patel <91098462+JayPatel092001@users.noreply.github.com>
2021-11-11 12:13:26 -0500

    Add files via upload
    all added

---
+++ Assignment-6/js/accordion.js
@@ -0,0 +1,12 @@
+$(function () {
+    $('.accordion').on('click', '.accordion-control', function (e) {
+        console.log("event data", e);
+        e.preventDefault();
+        
+        $(this)
+            .toggleClass('active')
+            .next('.accordion-panel')
+            .not('animated')
+            .slideToggle(1000);
+    });
+});
